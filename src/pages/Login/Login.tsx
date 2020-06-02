d65eb5b12aab99aeac6c8e44589824cc8cea9543
nolliebigspin <alec96.w@gmail.com>
2020-06-02 19:30:46 +0200

    added ioncard to input + styling space buttons

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,6 +3,8 @@ import { IonContent, IonPage, IonButton, IonCard, IonCardHeader, IonCardTitle, I
 import { Header } from '../../common/components';
 import { useHistory } from 'react-router';
 
+import './Login.css'
+
 const LIST_OF_USERS = ["Alec", "Alex", "Leonard", "Rafael"];
 
 const Login: React.FC = () => {
@@ -35,22 +37,27 @@ const Login: React.FC = () => {
           <IonCardContent>
             Wir bieten dir die Möglichkeit deine Nachbarn kennenzulernen und dich mit ihnen zu vernetzen.  
             Es ist Egal wie alt du bist oder welche Fähigkeiten du hast, trage deinen Teil zu einem angenehmen und hilfbereiten Miteinander bei.
-
-            Folgende Möglichkeiten bieten wir dir:
-            1) Gegenstände Leihen/Verleihen
-            2) Hilfe anfragen/anbieten
-            3) Wissensaustausch
+            <br/>
+            <br/>
+            <p>Folgende Möglichkeiten bieten wir dir:</p>
+            <p>1) Gegenstände Leihen/Verleihen</p>
+            <p>2) Hilfe anfragen/anbieten</p>
+            <p>3) Wissensaustausch</p>
           </IonCardContent>
         </IonCard>
-        <IonInput value={userName} placeholder="Benutzername" onIonChange={e => setUserName(e.detail.value!)}></IonInput>
-        <IonInput value={password} placeholder="Passwort" type="password" onKeyDown={e => handleKeyDown(e)} onIonChange={e => setPassword(e.detail.value!)}></IonInput>
+        <IonCard color="light">
+          <IonInput className="Input" value={userName} placeholder="Benutzername" onIonChange={e => setUserName(e.detail.value!)}></IonInput>
+        </IonCard>
+        <IonCard color="light">
+          <IonInput className="Input" value={password} placeholder="Passwort" type="password" onKeyDown={e => handleKeyDown(e)} onIonChange={e => setPassword(e.detail.value!)}></IonInput>
+        </IonCard>
         <IonGrid>
           <IonRow>
             <IonCol>
-            <IonButton color="secondary" onClick={checkLogin}>Einloggen</IonButton>
+              <IonButton className="Button" color="secondary" onClick={checkLogin}>Einloggen</IonButton>
             </IonCol>
             <IonCol>
-            <IonButton color="secondary" routerLink="/home">Registrieren</IonButton>
+              <IonButton className="Button" color="secondary" routerLink="/home">Registrieren</IonButton>
             </IonCol>
           </IonRow>
         </IonGrid>
