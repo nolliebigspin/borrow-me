42ff25cafd613c3fdec77c26ac6460fed931bf5a
lwagner <leonardt.wagner@student.uni-luebeck.de>
2020-06-13 19:11:03 +0200

    added an action button and an uncomplete version of the message send bar

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { IonContent, IonPage } from '@ionic/react';
 import { Header, MenuBar } from '../../common/components';
+import SendMessageField from '../../common/components/SendMessageField/SendMessageField';
+
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <Header title={"BorrowMe"} />
       <IonContent color="light">
+      <SendMessageField/>
       </IonContent>
       <MenuBar />
     </IonPage>
