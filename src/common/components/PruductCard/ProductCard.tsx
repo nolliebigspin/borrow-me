09d7b04a13f0a780448c2d834efa09f59d82215e
nolliebigspin <alec96.w@gmail.com>
2020-06-15 13:00:43 +0200

    added ProductCard component

---
+++ src/common/components/PruductCard/ProductCard.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './ChatBubble.css'
+import { IonAvatar, IonGrid, IonRow, IonIcon } from '@ionic/react';
+import { thumbsUp, thumbsUpOutline } from 'ionicons/icons';
+
+
+interface ContainerProps {
+
+}
+
+const ProductCard: React.FC<ContainerProps> = () => {
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { IonContent, IonPage } from '@ionic/react';
 import { Header, MenuBar } from '../../common/components';
+import ProductCard from '../../common/components/PruductCard/ProductCard';
 
 
 const Home: React.FC = () => {
@@ -8,6 +9,7 @@ const Home: React.FC = () => {
     <IonPage>
       <Header title={"BorrowMe"} />
       <IonContent color="light">
+        <ProductCard />
       </IonContent>
       <MenuBar />
     </IonPage>
