9745a03446f53fdf31fa96a3e20c0cfc65ac1ccb
nolliebigspin <alec96.w@gmail.com>
2020-06-15 16:23:49 +0200

    fixed router handling

--- src/common/components/MenuBar/MenuBar.tsx
+++ src/common/components/MenuBar/MenuBar.tsx
@@ -7,43 +7,26 @@ import {
   IonTabButton,
 } from '@ionic/react';
 
-import { IonReactRouter } from '@ionic/react-router';
 import { person, home, chatbubbles, mail } from 'ionicons/icons';
 
 
 const MenuBar: React.FC = () => {
 
-  // router workaround
-  const openFeed = (): void => {
-    window.location.assign('/feed');
-  }
-  const openHome = (): void => {
-    window.location.assign('/home');
-  }
-  const openProfile = (): void => {
-    window.location.assign('/profile');
-  }
-  const openMessages = (): void => {
-    window.location.assign('/messages');
-  }
-
   return (
-    <IonReactRouter>
-      <IonTabBar className="menuBar" slot="bottom">
-        <IonTabButton tab="profile" href="/profile" onClick={openProfile}>
-          <IonIcon icon={person} />
-        </IonTabButton>
-        <IonTabButton tab="home" href="/home" onClick={openHome}>
-          <IonIcon icon={home} />
-        </IonTabButton>
-        <IonTabButton tab="feed" href="/feed" onClick={openFeed}>
-          <IonIcon icon={chatbubbles} />
-        </IonTabButton>
-        <IonTabButton tab="messages" href="/messages" onClick={openMessages}>
-          <IonIcon icon={mail} />
-        </IonTabButton>
-      </IonTabBar>
-    </IonReactRouter>
+    <IonTabBar className="menuBar" slot="bottom">
+      <IonTabButton tab="profile" href="/profile">
+        <IonIcon icon={person} />
+      </IonTabButton>
+      <IonTabButton tab="home" href="/home">
+        <IonIcon icon={home} />
+      </IonTabButton>
+      <IonTabButton tab="feed" href="/feed">
+        <IonIcon icon={chatbubbles} />
+      </IonTabButton>
+      <IonTabButton tab="messages" href="/messages">
+        <IonIcon icon={mail} />
+      </IonTabButton>
+    </IonTabBar>
   );
 };
 
