930972d6bd12a5279de205260fee582139b4804a
nolliebigspin <alec96.w@gmail.com>
2020-06-15 16:15:05 +0200

    added question button compntent file

---
+++ src/common/components/QuestionButton/QuestionButton.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ProductFull.css'
+import { IonCard } from '@ionic/react';
+
+const QuestionButton: React.FC= () => {
+
+  return (
+    <IonCard color="light">
+  
+    </IonCard>
+  );
+};
+
+export default QuestionButton;
