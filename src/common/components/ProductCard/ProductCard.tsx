c9db2afe712d6bcd2f63913607ac0d6b234701d4
Rafael Wortmann <rafael.wortmann@student.uni-lubeck.de>
2020-06-16 23:20:43 +0200

    added modal to productCards

--- src/common/components/ProductCard/ProductCard.tsx
+++ src/common/components/ProductCard/ProductCard.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ProductCard.css'
-import { IonCard, IonCardTitle, IonCardContent, IonGrid, IonRow, IonImg, IonCol, IonChip, IonLabel, IonText } from '@ionic/react';
+import { IonCard, IonCardTitle, IonCardContent, IonGrid, IonRow, IonImg, IonCol, IonChip, IonLabel, IonText, IonModal, IonContent } from '@ionic/react';
+import { Header, MenuBar } from '../';
+import ActionButton from '../ActionButton/ActionButton';
 
 interface props {
   title: string;
@@ -11,41 +13,88 @@ interface props {
 }
 
 const ProductCard: React.FC<props> = ({ title, image, description, category, distance }) => {
+  
+  const [showModal, setShowModal] = useState(false);
 
   return (
-    <IonCard color="light">
-      <IonGrid>
-        <IonRow>
-          <IonCol>
-            <IonImg className="image" src={image} />
-          </IonCol>
-          <IonCol>
-            <div>
-              <IonCardTitle className="title">{title}</IonCardTitle>
-              <IonCardContent>
-                <IonRow>
-                  <IonCol>
-                    {category && 
-                      <IonChip color="secondary">
-                        <IonLabel>{category}</IonLabel>
-                      </IonChip>
-                    }
-                  </IonCol>
-                  <IonCol>
-                    {distance &&
-                      <IonChip color="secondary">
-                        <IonLabel>{distance} km</IonLabel>
-                      </IonChip>
-                    }
-                  </IonCol>
-                </IonRow>
-                {description && <IonText>{description}</IonText>}
-              </IonCardContent>
+    <div>
+      <div>
+        <IonCard color="light" onClick={() => setShowModal(true)}>
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonImg className="image" src={image} />
+              </IonCol>
+              <IonCol>
+                <div>
+                  <IonCardTitle className="title">{title}</IonCardTitle>
+                  <IonCardContent>
+                    <IonRow>
+                      <IonCol>
+                        {category && 
+                          <IonChip color="secondary">
+                            <IonLabel>{category}</IonLabel>
+                          </IonChip>
+                        }
+                      </IonCol>
+                      <IonCol>
+                        {distance &&
+                          <IonChip color="secondary">
+                            <IonLabel>{distance} km</IonLabel>
+                          </IonChip>
+                        }
+                      </IonCol>
+                    </IonRow>
+                    {description && <IonText>{description}</IonText>}
+                  </IonCardContent>
+                </div>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonCard>
+      </div>
+      <IonModal isOpen={showModal}>
+        <Header title={"BorrowMe"}/>
+        <IonContent>
+          <div>
+            <div className="card-title">
+              <IonCardTitle className="title-modal" color="primary" >{title}</IonCardTitle>
             </div>
-          </IonCol>
-        </IonRow>
-      </IonGrid>
-    </IonCard>
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonImg className="image" src={image} />
+              </IonCol>
+              <IonCol>
+                <div>
+                  <IonCardContent>
+                    <IonRow>
+                      <IonCol>
+                        {category && 
+                          <IonChip color="secondary">
+                            <IonLabel>{category}</IonLabel>
+                          </IonChip>
+                        }
+                      </IonCol>
+                      <IonCol>
+                        {distance &&
+                          <IonChip color="secondary">
+                            <IonLabel>{distance} km</IonLabel>
+                          </IonChip>
+                        }
+                      </IonCol>
+                    </IonRow>
+                    {description && <IonText>{description}</IonText>}
+                  </IonCardContent>
+                </div>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+          </div>
+        </IonContent>
+        <MenuBar/>
+      </IonModal>
+    </div>
   );
 };
 
--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IonContent, IonPage, IonText, IonItem, IonCard, IonInput, IonLabel, IonModal, IonButton, IonTitle} from '@ionic/react';
 import { Header, MenuBar} from '../../common/components';
 import ProductCard from '../../common/components/ProductCard/ProductCard';
@@ -6,21 +6,27 @@ import './Search.css'
 import SearchBar from '../../common/components/SearchBar/SearchBar';
 
 const Search: React.FC = () => {
+ 
   return (
     <IonPage>
       <Header title={"BorrowMe"} />
-      <IonContent color="light" className="content-container">
-        <div className="content-container">
-          <div className="searchbar">
+      <div className="searchbar">
             <SearchBar/>
+      </div> 
+      <IonContent color="light" className="content-container">
+      
+        <div className="content-container">
+          <div>
+            <ProductCard
+              title="Bohrmaschine"
+              image="https://images.unsplash.com/photo-1572981779307-38b8cabb2407?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80"
+              description="Eine tolle und gelbe Bohrmaschine"
+              distance="1"
+              category="Werkzeug"
+            />
           </div>
-            <ProductCard 
-                title="Bohrmaschine"
-                image="https://images.unsplash.com/photo-1572981779307-38b8cabb2407?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80"
-                description="Eine tolle und gelbe Bohrmaschine"
-                distance="1"
-                category="Werkzeug"
-              />
+          
+          <div>
             <ProductCard 
                 title="Bohrmaschine"
                 image="https://images.unsplash.com/photo-1564942513776-e922cb937bfe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80"
@@ -28,19 +34,27 @@ const Search: React.FC = () => {
                 distance="4"
                 category="Werkzeug"
               />
+          </div>
+          
+          <div>
             <ProductCard 
               title="Bohrmaschine"
               image="https://images.unsplash.com/photo-1572981779307-38b8cabb2407?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80"
               description="Eine tolle und gelbe Bohrmaschine"
               category="Werkzeug"
-            />
+            />  
+          </div>
+          
+          <div>
             <ProductCard 
-                title="Kinder Bohrmaschine"
-                image="https://images.unsplash.com/photo-1551631880-e807a4453bc1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=500&q=80"
-                description="Meine Kinder spielen nur noch sehr selten damit und ich will sie auch nicht einstauben lassen"
-                distance="3"
-                category="Spielzeug"
-              />
+              title="Kinder Bohrmaschine"
+              image="https://images.unsplash.com/photo-1551631880-e807a4453bc1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=500&q=80"
+              description="Meine Kinder spielen nur noch sehr selten damit und ich will sie auch nicht einstauben lassen"
+              distance="3"
+              category="Spielzeug"
+            />
+          </div>
+          
         </div>
       </IonContent>
       <MenuBar />
