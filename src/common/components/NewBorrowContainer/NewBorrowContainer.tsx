950ec58ebc14b1e14004310f551ae8610121ad08
nolliebigspin <alec96.w@gmail.com>
2020-06-17 01:23:53 +0200

    added files for NewBorrowContainer

---
+++ src/common/components/NewBorrowContainer/NewBorrowContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NewBorrowContainer.css';
+
+const NewBorrowContainer: React.FC = () => {
+
+  return (
+    <div className="base">
+
+    </div>
+  );
+};
+
+export default NewBorrowContainer;
