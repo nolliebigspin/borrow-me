8644990a88ce10b6057d5aa641cd238ce43c0c13
Rafael Wortmann <rafael.wortmann@student.uni-lubeck.de>
2020-06-16 20:16:34 +0200

    finished the the first search page

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -3,19 +3,45 @@ import { IonContent, IonPage, IonText, IonItem, IonCard, IonInput, IonLabel, Ion
 import { Header, MenuBar} from '../../common/components';
 import ProductCard from '../../common/components/ProductCard/ProductCard';
 import './Search.css'
+import SearchBar from '../../common/components/SearchBar/SearchBar';
 
 const Search: React.FC = () => {
   return (
     <IonPage>
       <Header title={"BorrowMe"} />
       <IonContent color="light" className="content-container">
-        <ProductCard 
-            title="Bohrmaschine"
-            image="https://images.unsplash.com/photo-1572981779307-38b8cabb2407?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80"
-            description="Eine tolle und gelbe Bohrmaschine"
-            distance="3"
-            category="Werkzeug"
-          />
+        <div className="content-container">
+          <div className="searchbar">
+            <SearchBar/>
+          </div>
+            <ProductCard 
+                title="Bohrmaschine"
+                image="https://images.unsplash.com/photo-1572981779307-38b8cabb2407?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80"
+                description="Eine tolle und gelbe Bohrmaschine"
+                distance="1"
+                category="Werkzeug"
+              />
+            <ProductCard 
+                title="Bohrmaschine"
+                image="https://images.unsplash.com/photo-1564942513776-e922cb937bfe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80"
+                description="Eine tolle und rote Bohrmaschine"
+                distance="4"
+                category="Werkzeug"
+              />
+            <ProductCard 
+              title="Bohrmaschine"
+              image="https://images.unsplash.com/photo-1572981779307-38b8cabb2407?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80"
+              description="Eine tolle und gelbe Bohrmaschine"
+              category="Werkzeug"
+            />
+            <ProductCard 
+                title="Kinder Bohrmaschine"
+                image="https://images.unsplash.com/photo-1551631880-e807a4453bc1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=500&q=80"
+                description="Meine Kinder spielen nur noch sehr selten damit und ich will sie auch nicht einstauben lassen"
+                distance="3"
+                category="Spielzeug"
+              />
+        </div>
       </IonContent>
       <MenuBar />
     </IonPage>
